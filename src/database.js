c1ea7923473c776ba1aefaf12aa8eb749a05a576
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-02-01 15:34:36 +0200

    .

---
+++ src/database.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize(PG_DB, PG_USER, PG_PASSWORD, {
+  host: PG_HOST,
+  port: PG_PORT,
+  dialect: "postgres"
+});
+
+export const pgConnect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+    process.exit(1);
+  }
+};
+
+export const syncModels = async () => {
+  await sequelize.sync({ alter: true });
+  console.log("All models were synchronized successfully.");
+};
